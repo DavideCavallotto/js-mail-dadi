309e70769cc0faab59cc33bb77e6e3ad3912512f
DavideCavallotto <davidecavallotto92@gmail.com>
2023-10-09 16:46:01 +0200

    add result message

--- app.js
+++ app.js
@@ -5,10 +5,12 @@ const listMail = [
     'alpha@gmail.com', 
     'bravo@gmail.com', 
     'charlie@gmail.com', 
-    '', 
+    'delta@gmail.com', 
     'echo@gmail.com', 
     'foxtrot@gmail.com',   
 ]
+
+console.log(listMail)
 
     // - creo una variabile che mi dica quante è lungo l'Array
 const lunghArray = listMail.length;
@@ -17,20 +19,30 @@ console.log(lunghArray)
 // - chiedo la mail all'utente
     // - tramite un prompt chiede all'utente di inserire la sua email
 let mailUser = prompt("Inserisci la tua email");
-console.log(mailUser);
+console.log(mailUser, 'mail inserita dall\'utente');
 
+let currentMail;
+// - dichiaro una variabile trovato uguale a false
+let trovato = false;
 // - creo un ciclo for per confrontare la mail inserita con quelle del ciclo
 for (let i = 0; i < lunghArray; i++ ) {
-    const currentMail = listMail[i]    
+    currentMail = listMail[i];
 
     if (mailUser === currentMail) {
-        console.log(currentMail, 'passed')
-        
-    } else if (mailUser !== currentMail)  {
-        console.log(currentMail, 'unpassed')
-        
-    } 
+        trovato = true;
+    }
    
+}
+
+// currentMail = 'foxtrot@gmail.com';
+// - se la variabile 'trovato' è uguale a 'true'
+//     - stampo passed
+if (trovato === true) {
+    console.log('passed');
+} else {
+    // - altrimenti
+    //     - stampo unpassed 
+    console.log('unpassed');
 }
 
 
--- readme.md
+++ readme.md
@@ -4,4 +4,10 @@
     - creo una variabile che mi dica quante è lungo l'Array
 - chiedo la mail all'utente
     - tramite un prompt chiede all'utente di inserire la sua email
+- dichiaro una variabile 'trovato' uguale a 'false'
 - creo un ciclo for per confrontare la mail inserita con quelle del ciclo
+- confrontato il risultato
+    - se la variabile 'trovato' è uguale a 'true'
+        - stampo passed
+    - altrimenti
+        - stampo unpassed 
